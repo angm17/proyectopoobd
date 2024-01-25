3479df22d5e7fb0b2a0860d63ce9b7d44ac69407
FAMILYHOLGUIN\Family <brsholguin@est.istg.edu.ec>
2024-01-25 02:22:38 -0500

    creo que sirve - ventana de puntaje

---
+++ fuente/Puntaje (3).py
@@ -0,0 +1,49 @@
+from tkinter import *
+from tkinter import font
+from PIL import Image, ImageTk
+
+# Crea la ventana
+ventana = Tk()
+ventana.title("Ventana de puntajes")
+ventana.geometry("{0}x{1}+0+0".format(ventana.winfo_screenwidth(), ventana.winfo_screenheight()))
+
+# Carga la imagen de fondo
+ruta_imagen = "C:/Users/Family/Downloads/image 3.png"
+imagen_fondo = Image.open(ruta_imagen)
+imagen_fondo = imagen_fondo.resize((ventana.winfo_screenwidth(), ventana.winfo_screenheight()), Image.LANCZOS)
+imagen_tk = ImageTk.PhotoImage(imagen_fondo)
+
+# Definir la fuente personalizada
+fuente = font.Font(family="MS Sans Serif", size=34, weight="bold")
+fuente_boton = font.Font(family="Fixedsys", size=28, weight="bold")
+
+
+canvas = Canvas(ventana, width=ventana.winfo_screenwidth(), height=ventana.winfo_screenheight())
+canvas.pack(side="top", fill="both", expand=True)
+canvas.create_image(0, 0, image=imagen_tk, anchor="nw")
+
+
+# Crear el marco (frame) para el recuadro
+marco = Frame(ventana, bg="#CDFFF6", width=550, height=340)
+marco.pack(pady=10)
+
+
+# Crear el título del recuadro
+titulo = Label(text="Felicidades!!!", font=fuente, bg="#CDFFF6", fg="yellow")
+titulo.place(relx=0.5, rely=0.1, anchor="center")
+
+# Crear el texto del recuadro
+texto = Label(text="Tu puntaje es: 1000\nTiempo restante: 5 segundos\nCartas acertadas: 10\nIntentos fallidos: 1", font=("Arial", 28), bg="#CDFFF6", fg="blue")
+texto.place(relx=0.5, rely=0.4, anchor="center")
+
+# Crear el botón "Continuar"
+boton = Button(text="Continuar", font=fuente_boton, bg="#89E97F", fg="#FFFFFF", command=lambda: print("Botón presionado"))
+boton.place(relx=0.5, rely=0.8, anchor="center")
+
+# Función para abrir la otra ventana
+def otra_ventana():
+    # Crea la otra ventana aquí
+    pass
+
+# Inicia el ciclo de la ventana
+ventana.mainloop()
